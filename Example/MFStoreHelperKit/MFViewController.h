87362a9e326f69a86e698f47f5c64da5aa94c9a5
feng.ma <feng.ma@patpat.com>
2018-12-27 11:24:54 +0800

    Initial commit

---
+++ Example/MFStoreHelperKit/MFViewController.h
@@ -0,0 +1,13 @@
+//
+//  MFViewController.h
+//  MFStoreHelperKit
+//
+//  Created by 1499603656@qq.com on 12/27/2018.
+//  Copyright (c) 2018 1499603656@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MFViewController : UIViewController
+
+@end
---
+++ Example/MFStoreHelperKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MFStoreHelperKit
+//
+//  Created by 1499603656@qq.com on 12/27/2018.
+//  Copyright (c) 2018 1499603656@qq.com. All rights reserved.
+//
+
+@import UIKit;
+#import "MFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFAppDelegate class]));
+    }
+}
